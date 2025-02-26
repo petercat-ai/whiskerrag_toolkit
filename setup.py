f9d9dff77c4d56931b8f342fc3171cb8044c6e53
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-02-26 14:23:53 +0800

    Release version 0.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="whiskerrag",
-    version="0.0.9",
+    version="0.0.10",
     description="A utility package for RAG operations",
     long_description=README,
     long_description_content_type="text/markdown",
