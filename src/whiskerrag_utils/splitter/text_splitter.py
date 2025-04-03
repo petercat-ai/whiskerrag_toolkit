40496354e59fda409a8f24b0aeeb90015063268c
ch-liuzhide <liuzhide.lzd@antgroup.com>
2025-04-03 11:05:05 +0800

    fix(textsplitconfig): fix deserialization issues caused by optional parameters in model

--- src/whiskerrag_utils/splitter/text_splitter.py
+++ src/whiskerrag_utils/splitter/text_splitter.py
@@ -9,22 +9,19 @@ from whiskerrag_types.model.multi_modal import Text
 from whiskerrag_utils.registry import RegisterTypeEnum, register
 
 
-def separators_to_regex(separators: list[str]) -> str:
-    escaped_separators = [re.escape(sep) for sep in separators]
-    pattern = "|".join(escaped_separators)
-    return pattern
-
-
 @register(RegisterTypeEnum.SPLITTER, KnowledgeTypeEnum.TEXT)
 class TextSplitter(BaseSplitter[TextSplitConfig, Text]):
     def split(self, content: str, split_config: TextSplitConfig) -> List[Text]:
+        config_dict = split_config.model_dump(mode="json")
+        separators = config_dict["separators"] or ["\n\n"]
+        separator_pattern = "|".join(map(re.escape, separators))
         splitter = CharacterTextSplitter(
-            chunk_size=split_config.chunk_size,
-            chunk_overlap=split_config.chunk_overlap,
-            separator=separators_to_regex(split_config.separators or ["\n\n"]),
-            is_regex=True,
-            keep_separator=split_config.keep_separator,
-            strip_whitespace=split_config.strip_whitespace,
+            chunk_size=config_dict["chunk_size"],
+            chunk_overlap=config_dict["chunk_overlap"],
+            separator=separator_pattern,
+            is_separator_regex=True,
+            keep_separator=config_dict["keep_separator"],
+            strip_whitespace=config_dict["strip_whitespace"],
         )
         return splitter.split_text(content)
 
